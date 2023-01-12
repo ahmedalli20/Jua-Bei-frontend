e85d7b8583b70542d48e1ffbe51318dee83c73f3
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2023-01-12 09:43:26 +0300

    created the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import './Home.css';
+import Navbar from './Navbar';
 
 function Home() {
   return (
     <div className="home">
-    
-    
+       <Navbar />
+         <div className='homes'>
+
+
+         </div>
     </div>
   );
 }
