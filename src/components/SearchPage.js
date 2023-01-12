a2b7edf1b5823d3d746765583ee8ece02fd6b92b
ahmedalli20 <ahmed.alistudent.moringaschool.com>
2023-01-12 09:40:04 +0300

    created search component

---
+++ src/components/SearchPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SearchPage.css'
+
+const SearchPage = ({handleSearch,search,setSearch}) => {
+    return (
+        <div className="searchpage">
+            <div className='search'>
+                <div>
+                    <div></div>
+                </div>
+                <form onSubmit={handleSearch} className='search-form' type="submit">
+                    <button><img  id='search' src="../icons/search .svg" /></button>
+                    <input value={search} onChange={(e) => setSearch(e.target.value)} type={"text"} placeholder="Searching" />
+                    {search === "" ? null : <label onClick={() => setSearch("")}><img id='cancel' src='../icons/cancel.svg' /></label>}
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default SearchPage;
+
